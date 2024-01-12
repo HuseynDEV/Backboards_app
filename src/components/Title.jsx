d087d4f8de68077f8bb818e7139d9a464e1fc929
Huseyn Alizade <huseyna@maestro.st>
2024-01-12 23:17:08 +0400

    added text in Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = () => {
+    return (
+      <div className='section-title'>
+        <h2>
+          Welcome <span>everyone</span>
+        </h2>
+      </div>
+    );
+  };
+  export default Title;
+  
