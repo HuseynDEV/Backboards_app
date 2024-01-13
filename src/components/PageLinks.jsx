109eb6137df79beee7c4873d3dc937e210012824
Huseyn Alizade <huseyna@maestro.st>
2024-01-13 22:35:15 +0400

    created pagelink

--- src/components/PageLinks.jsx
+++ src/components/PageLinks.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { pageLinks } from '../data'
 
 const PageLinks = () => {
   return (
-    <div>PageLinks</div>
+    <ul className={parentClass} id='nav-links'>
+    {pageLinks.map((link) => {
+      return <PageLink key={link.id} link={link} itemClass={itemClass} />
+    })}
+  </ul>
   )
 }
 
