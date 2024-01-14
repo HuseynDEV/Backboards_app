2b704c880e34be2497a59c1a2076f0cf4724c440
Huseyn Alizade <huseyna@maestro.st>
2024-01-14 23:17:12 +0400

    added data in Tours page and called Tour Component

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {tours} from '../data'
+import Title from './Title'
 
 const Tours = () => {
   return (
-    <div>Tours</div>
+    <section className='section' id='tours'>
+      <Title title='featured' subTitle='tours' />
+
+      <div className='section-center featured-center'>
+        {tours.map((tour) => {
+          return <Tour {...tour} key={tour.id} />
+        })}
+      </div>
+    </section>
   )
 }
 
