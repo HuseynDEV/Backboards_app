ed9a9706c5ac2f59ac49609f60cea2f2f2c02c4b
Huseyn Alizade <huseyna@maestro.st>
2024-01-16 21:09:21 +0400

    added component in App js file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import About from './components/ABout'
 import Tours from './components/Tours'
 import Footer from './components/Footer'
+import Services from './components/Services'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <About />
+      <Navbar/>
       <Hero />
+      <About />
+      <Services/>
       <Tours/>
       <Footer/>
     </div>
